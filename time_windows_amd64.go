4e5ab8355b939ba1a94b8c554e2f179b4fda6713
Hysios Hu <hysios@gmail.com>
2023-02-06 17:45:04 +0800

    Fixed: SetSystemDate() on Windows

--- time_windows_amd64.go
+++ time_windows_amd64.go
@@ -7,13 +7,13 @@ import (
 )
 
 func SetSystemDate(newTime time.Time) error {
-	w32.SetSystemTime(w32.SystemTime{
-		Year:         newTime.Year(),
-		Month:        newTime.Month(),
-		Day:          newTime.Day(),
-		Hour:         newTime.Hour(),
-		Minute:       newTime.Minute(),
-		Second:       newTime.Second(),
+	w32.SetSystemTime(w32.SYSTEMTIME{
+		Year:         uint16(newTime.Year()),
+		Month:        uint16(newTime.Month()),
+		Day:          uint16(newTime.Day()),
+		Hour:         uint16(newTime.Hour()),
+		Minute:       uint16(newTime.Minute()),
+		Second:       uint16(newTime.Second()),
 		Milliseconds: 0,
 	})
 	return nil
